db1d7cad9b8ee62ece518713ce2a56e97e5ab652
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-10-26 14:41:11 +0200

    create file structure/screen layout, tabs with Home and Profile screen

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Tabs } from 'expo-router';
+
+const TabsLayout = () => {
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default TabsLayout;
