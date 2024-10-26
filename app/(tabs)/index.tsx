c80f76b3e3247b6a65253b7d496e6505f33308c6
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-10-26 14:54:37 +0200

    create dynamic route postDetails/[id]

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,29 @@
-import { View, Text } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import { Link } from 'expo-router';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={styles.container}>
+      <Link href="/postDetails/1">Link</Link>
+      <Link
+        href={{
+          pathname: '/postDetails/[id]',
+          params: { id: 'banana' },
+        }}
+      >
+        Link 2
+      </Link>
     </View>
   );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ app/postDetails/[id].tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { useLocalSearchParams } from 'expo-router';
+
+const PostDetails = () => {
+  const { id } = useLocalSearchParams();
+  return (
+    <View>
+      <Text>ID: {id}</Text>
+    </View>
+  );
+};
+
+export default PostDetails;
