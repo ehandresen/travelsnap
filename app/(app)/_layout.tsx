0547327c806f46b6964f5bffd404c5d189914f7c
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-11-01 18:52:41 +0100

    inn AppLayout redirect depending on usernanameSession from authcontext

--- app/(app)/_layout.tsx
+++ app/(app)/_layout.tsx
@@ -1,7 +1,23 @@
+import { useAuthSession } from '@/context/AuthContext';
 import { PostProvider } from '@/context/PostContext';
-import { Stack } from 'expo-router';
+import { Redirect, Stack } from 'expo-router';
+import { Text, View } from 'react-native';
 
 export default function AppLayout() {
+  const { usernameSession, isLoading } = useAuthSession();
+
+  if (isLoading) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>loading...</Text>
+      </View>
+    );
+  }
+
+  if (!usernameSession) {
+    return <Redirect href="/authentication" />;
+  }
+
   return (
     <PostProvider>
       <Stack>
