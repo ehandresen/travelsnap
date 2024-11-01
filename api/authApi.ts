bf128a2ffbfd3858a8e9fcca44a55990622ce4c1
Eirik Andresen <eirik.horgen.andresen@gmail.com>
2024-11-01 22:29:06 +0100

    add signOut ti authApi

--- api/authApi.ts
+++ api/authApi.ts
@@ -3,6 +3,10 @@ import { auth } from '@/firebaseConfig';
 
 export const signIn = async (email: string, password: string) => {
   await signInWithEmailAndPassword(auth, email, password)
-    .then((userCredential) => console.log(`user signed in: ${userCredential}`))
-    .catch((error) => console.log(`could not sign in ${error}`));
+    .then((userCredential) => console.log(`user signed in`, userCredential))
+    .catch((error) => console.log(`could not sign in, ${error}`));
+};
+
+export const signOut = async () => {
+  await auth.signOut().then(() => console.log('successfully signed out'));
 };
